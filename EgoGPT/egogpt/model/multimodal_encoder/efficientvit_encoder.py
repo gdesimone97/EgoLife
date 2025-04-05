c1a1fb8309f931168cd60c41dda56efd706829ef
gdesimone <gds2011@libero.it>
2025-04-05 20:56:48 +0200

    up

--- EgoGPT/egogpt/model/multimodal_encoder/efficientvit_encoder.py
+++ EgoGPT/egogpt/model/multimodal_encoder/efficientvit_encoder.py
@@ -3,10 +3,14 @@ import torch
 from torch import nn
 from efficientvit.cls_model_zoo import create_efficientvit_cls_model
 from pathlib import Path
+from transformers import ViTImageProcessor
+import re
 
 class EfficientVit(nn.Module):
-    def __init__(self, weitghts_name="efficientvit_l3_r320"):
+    def __init__(self, weitghts_name="efficientvit_l3_r320", device="cuda"):
         super().__init__()
+        self.device = device
+        self.build_processor(weitghts_name)
         base_path = Path(os.environ["PYTHONPATH"])
         weights_root = "models/efficient_vit/"
         if not weitghts_name.endswith(".pt"):
@@ -17,8 +21,30 @@ class EfficientVit(nn.Module):
             name=model_name,
             weight_url=weight_url
         )
+        self.gap = nn.AdaptiveAvgPool2d(output_size=1)
+        print(self.model)
+        self.model.head = nn.Identity()
+        self.model = self.model.to(device=device)
+    
+    def build_processor(self, model_name):
+        size = re.findall(r"_r(\d+)", model_name)[0]
+        self._processor = ViTImageProcessor(
+            size={"width": size, "height": size}
+        )
+    
+    def forward(self, image):
+        image = image.to(self.device)
+        emb = self.model(image)["stage_final"]
+        emb = self.gap(emb)
+        return emb
+    
+    @property
+    def processor(self):
+        return self._processor
 
 if __name__ == "__main__":
     model = EfficientVit()
-    model.model.head = nn.Identity()
-    print(model)
+    model.eval()
+    x = torch.rand(1, 3, 320, 320)
+    out = model(x)
+    print(out.shape)
